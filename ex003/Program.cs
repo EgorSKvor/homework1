2e55ea8d4cad10358faa5f780d51e5b331f0378b
Egor <ee3068316@gmail.com>
2022-09-04 22:18:45 +0300

    did ex

---
+++ ex003/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Введите число, а магия определить четное оно или нет ");
+string name = Console.ReadLine();
+int number = Convert.ToInt32(name);
+if (number % 2 == 0)
+{
+    System.Console.WriteLine("Ваше число четное");
+} 
+else 
+{
+    System.Console.WriteLine("Ваше число нечетное");
+}    
