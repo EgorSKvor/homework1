ba01ea3d8369637cad4b970626ddd993f8c0bdf7
Egor <ee3068316@gmail.com>
2022-09-04 22:18:39 +0300

    did ex

---
+++ ex002/Program.cs
@@ -0,0 +1,30 @@
+﻿System.Console.WriteLine("Введите первое число");
+string name = Console.ReadLine();
+int num1 = Convert.ToInt32(name);
+
+System.Console.WriteLine("Введите первое число");
+string num2 = Console.ReadLine();
+int number = Convert.ToInt32(name);
+
+System.Console.WriteLine("Введите первое число");
+string name = Console.ReadLine();
+int num3 = Convert.ToInt32(name);
+maxNum = num1;
+if (num2 > maxNum) maxNum = num2;
+{
+    System.Console.WriteLine("Наибольшее из 3х = ");
+    Console.Write(maxNum);
+}
+else if (num3 > maxNum) maxNum = num3;
+{
+    System.Console.WriteLine("Наибольшее из 3х = ");
+    Console.Write(maxNum);
+}
+else 
+{
+    System.Console.WriteLine("Наибольшее из 3х = ");
+    Console.Write(maxNum);
+}
+
+
+
