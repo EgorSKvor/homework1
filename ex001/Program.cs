9a9a69695a74edb8210346bdbbe43b7470f748a9
Egor <ee3068316@gmail.com>
2022-09-04 22:18:22 +0300

    did ex

--- ex001/Program.cs
+++ ex001/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿int a = 5;
+int b = 7;
+if (a > b)
+{
+    System.Console.WriteLine("max = ");
+    Console.Write(a);
+}
+else 
+{
+    System.Console.WriteLine("max = ");
+    Console.Write(b);
+}
